b2f4cf7755c9a4c8233b919d2b1d7e58015c8e06
Ubuntu <ubuntu@ip-172-31-33-105.ap-northeast-2.compute.internal>
2023-10-31 04:39:30 +0000

    bug fixed

--- install-software.sh
+++ install-software.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
 # INITIALIZE APT
 sudo apt update
 sudo apt -y upgrade
@@ -9,7 +12,7 @@ cd /home/ubuntu
 sudo apt -y install git binutils
 git clone https://github.com/aws/efs-utils
 cd /home/ubuntu/efs-utils
-/home/ubuntu/build-deb.sh
+./build-deb.sh
 sudo apt-get -y install ./build/amazon-efs-utils*deb
 echo "fs-020ae87de07d938d0:/ /home/ubuntu/efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
 cd /home/ubuntu
@@ -21,3 +24,5 @@ rm -rf .ssh
 ln -s /home/ubuntu/efs/.ssh /home/ubuntu/.ssh
 ls -s /home/ubuntu/efs/.emacs.d /home/ubuntu/.emacs.d
 
+# SOFTWARE
+sudo apt -y install emacs
