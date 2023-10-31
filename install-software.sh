03998c3d184dd27b9dab5135a2b77eaf01d202c1
Ubuntu <ubuntu@ip-172-31-34-102.ap-northeast-2.compute.internal>
2023-10-31 05:55:52 +0000

    bug fixed

--- install-software.sh
+++ install-software.sh
@@ -7,6 +7,12 @@ set -e
 sudo apt update
 sudo apt -y upgrade
 
+
+# SOFTWARE
+cd /home/ubuntu
+sudo apt -y install emacs gcc make python3-pip
+
+
 # EFS
 cd /home/ubuntu
 sudo apt -y install git binutils
@@ -24,6 +30,31 @@ rm -rf /home/ubuntu/.ssh
 ln -s /home/ubuntu/efs/.ssh /home/ubuntu/.ssh
 ln -s /home/ubuntu/efs/.emacs.d /home/ubuntu/.emacs.d
 
-# SOFTWARE
+
+# TA LIB
 cd /home/ubuntu
-sudo apt -y install emacs
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvf ta-lib-0.4.0-src.tar.gz --no-same-owner
+rm ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+
+sudo ./configure
+sudo make
+sudo make install
+
+
+# TPQOA
+cd /home/ubuntu
+git clone https://github.com/yhilpisch/tpqoa
+cd tpqoa
+python setup.py install
+
+
+# PYTHON LIBRARIES
+cd /home/ubuntu
+pip install jupyter imageio
+pip install ccxt ta-lib
+pip install numpy pandas matplotlib seaborn
+pip install scikit-learn xgboost tensorflow
+
+
