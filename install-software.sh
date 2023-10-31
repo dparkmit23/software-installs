99e9c91f3014a0a0f6ea598e2962a2c3c7dfe049
Ubuntu <ubuntu@ip-172-31-8-228.ap-northeast-2.compute.internal>
2023-10-31 04:03:01 +0000

    changes

---
+++ install-software.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# INITIALIZE APT
+sudo apt update
+sudo apt -y upgrade
+
+# EFS
+cd /home/ubuntu
+sudo apt -y install git binutils
+git clone https://github.com/aws/efs-utils
+cd /home/ubuntu/efs-utils
+/home/ubuntu//build-deb.sh
+sudo apt-get -y install ./build/amazon-efs-utils*deb
+echo "fs-020ae87de07d938d0:/ /home/ubuntu/efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
+cd /home/ubuntu
+mkdir /home/ubuntu/efs
+sudo mount -a
+
+cd /home/ubuntu
+rm -rf .ssh
+ln -s /home/ubuntu/efs/.ssh /home/ubuntu/.ssh
+ls -s /home/ubuntu/efs/.emacs.d /home/ubuntu/.emacs.d
+
