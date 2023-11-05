f36289456936ca1f6315f887261df4490a952bf0
Ubuntu <ubuntu@ip-172-31-13-13.ap-northeast-2.compute.internal>
2023-11-05 20:00:08 +0000

    fixed bug

--- install-db.sh
+++ install-db.sh
@@ -3,7 +3,9 @@
 # Exit on error
 set -e
 
-db_dir="/home/ubuntu/efs/db"
+db_dir="/home/ubuntu/efs/mysql"
+efs_id = "fs-08489c99cb1a0c33a"
+efs_entry ="$efs_id:/ /home/ubuntu/efs efs _netdev,tls 0 0" 
 
 # INITIALIZE APT
 sudo apt update
@@ -11,19 +13,20 @@ sudo apt -y upgrade
 
 # EFS
 cd /home/ubuntu
-if [ ! -d "$db_dir" ]; then
+if grep -Fxq "$efs_entry" /etc/fstab; then
+    echo "Skipped creating link to EFS because it already exists."
+else
+    echo "Creating EFS link now."
     sudo apt -y install git binutils
     git clone https://github.com/aws/efs-utils
     cd /home/ubuntu/efs-utils
     ./build-deb.sh
     sudo apt-get -y install ./build/amazon-efs-utils*deb
-    echo "fs-08489c99cb1a0c33a:/ /home/ubuntu/efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
+    echo "$efs_id:/ /home/ubuntu/efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
     cd /home/ubuntu
     mkdir /home/ubuntu/efs
     sudo mount -a
     mkdir -p $db_dir
-else
-    echo "Skipped creating link to EFS because it already exists."
 fi
 
 # DB 
